71817491ea3a9f9c386efa3f00f033f3bef5aeda
Avishka <avishkakavishan13@gmail.com>
2026-06-24 20:06:24 +0530

    implement root layout and design system tokens for login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link rel="icon" href={`${basePath}/logo.png`} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Abhaya+Libre:wght@400;500;600;700;800&display=swap" />
       </head>
       <body className="antialiased">
         {children}
